607d6f0bdde7fc2cec9b0251fa75452d3878ff69
Daniel Ellis <1346302+dellis23@users.noreply.github.com>
2022-04-02 21:42:32 -0400

    Add theme song.

--- js/title.js
+++ js/title.js
@@ -1,5 +1,19 @@
 import * as game from './game.js';
 
+var theme_song = new Audio('sound/i sink not - song 1 - title.mp3');
+theme_song.addEventListener('timeupdate', function(){
+    var buffer = .44;
+    if(this.currentTime > this.duration - buffer){
+        this.currentTime = 0;
+        this.play();
+    }
+});
+
+var play_theme_button = document.getElementById('play_theme');
+play_theme_button.onclick = function () {
+	theme_song.play();
+};
+
 var main_song = new Audio('sound/i sink not - song 2.mp3');
 main_song.addEventListener('timeupdate', function(){
     var buffer = .44;
@@ -9,6 +23,11 @@ main_song.addEventListener('timeupdate', function(){
     }
 });
 
+function bezier(t)
+{
+    return 1 - (t * t * (3.0 - 2.0 * t));
+}
+
 export function setUp(canvasEl) {
     canvasEl.onclick = function() { onClick(canvasEl) };
 
@@ -17,10 +36,50 @@ export function setUp(canvasEl) {
     ctx.fillText('click anywhere to start', 100, 100);
 }
 
+var showing_title_screen = false;
+var game_started = false;
+
 function onClick(canvasEl) {
-    main_song.play();
-    tearDown(canvasEl);
-    game.setUp(canvasEl);
+
+	// Show title screen
+	if (!showing_title_screen && !game_started) {
+		theme_song.play();
+	    const ctx = canvasEl.getContext('2d');
+	    ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
+    	ctx.fillText('click anywhere to start (again)', 100, 100);
+	    ctx.font = "48pt arial";
+	    ctx.textAlign = "center";
+	    ctx.fillText('i sink not', 1024 / 2, 768 / 2);
+	    ctx.textAlign = "start";
+		showing_title_screen = true;
+	}
+
+	// Start game
+	else if (showing_title_screen && !game_started){
+
+		// Fade out title music, start main music
+		var current_t = 0.0;
+		var interval = 0.2;
+		var fade_time = 3.0;
+		var fadeAudio = setInterval(function () {
+	        if (theme_song.volume > 0.0) {
+	            theme_song.volume = bezier(current_t);
+	        }
+	        current_t += interval / fade_time;
+	        if (theme_song.volume <= 0.0) {
+	        	theme_song.pause();
+	            clearInterval(fadeAudio);
+	            setTimeout(function () {main_song.play();}, 1000)
+	        }
+	    }, interval * 1000);
+
+	    // Start the game
+	    showing_title_screen = false;
+	    game_started = true;
+	    tearDown(canvasEl);
+	    game.setUp(canvasEl);
+	}
+
 }
 
 function tearDown(canvasEl) {
