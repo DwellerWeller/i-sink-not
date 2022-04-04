b579ee287227b7d5b2eda4f9e249d704f3beb2df
Neil Williams <neil@reddit.com>
2022-04-03 20:39:43 -0700

    add text to initial loading card
    david was confused about what the play button meant

--- js/title.js
+++ js/title.js
@@ -8,7 +8,14 @@ export function setUp(canvasEl) {
     const ctx = canvasEl.getContext('2d');
     ctx.fillStyle = 'white';
     ctx.fillRect(0, 0, canvasEl.width, canvasEl.height);
+
     ctx.drawImage(window.playImg, canvasEl.width/2 - window.playImg.width/2, canvasEl.height/2 - window.playImg.height/2);
+
+    ctx.fillStyle = '#666';
+    ctx.font = '28px sans-serif';
+    const message = 'Click to start';
+    const textMetrics = ctx.measureText(message);
+    ctx.fillText(message, canvasEl.width/2 - textMetrics.width/2, canvasEl.height/2 + window.playImg.height/2 + 25 + textMetrics.actualBoundingBoxAscent)
 }
 
 function onClick(canvasEl) {
