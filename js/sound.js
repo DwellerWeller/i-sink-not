6af1d250362ce047c2181087bb134266a4bf82de
Daniel Ellis <1346302+dellis23@users.noreply.github.com>
2022-04-03 21:39:56 -0400

    Fix reference error.

--- js/sound.js
+++ js/sound.js
@@ -1,8 +1,8 @@
 export var building  = new Audio('sound/building.mp3');
-breaking.volume = 0.70;
+building.volume = 0.70;
 
 export var repairing  = new Audio('sound/repairing.mp3');
-breaking.volume = 0.70;
+repairing.volume = 0.70;
 
 export var breaking  = new Audio('sound/breaking.mp3');
 breaking.volume = 0.70;
